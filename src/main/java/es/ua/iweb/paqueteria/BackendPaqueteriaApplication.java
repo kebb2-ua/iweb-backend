cb97cc768aa13949590a343506203c1b6edc1db1
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2024-12-03 17:15:39 +0100

    Commit inicial

---
+++ src/main/java/es/ua/iweb/paqueteria/BackendPaqueteriaApplication.java
@@ -0,0 +1,13 @@
+package es.ua.iweb.paqueteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPaqueteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPaqueteriaApplication.class, args);
+    }
+
+}
