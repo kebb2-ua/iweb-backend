22223d0a63e77c307161757deed1d0e0047e24fa
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-12 16:05:07 +0100

    Fix emails

---
+++ src/main/java/es/ua/iweb/paqueteria/repository/DireccionRepository.java
@@ -0,0 +1,14 @@
+package es.ua.iweb.paqueteria.repository;
+
+import es.ua.iweb.paqueteria.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
+
+    Optional<UserEntity> findByVerificationToken(String token);
+}
--- src/main/java/es/ua/iweb/paqueteria/service/EmailingService.java
+++ src/main/java/es/ua/iweb/paqueteria/service/EmailingService.java
@@ -2,6 +2,7 @@ package es.ua.iweb.paqueteria.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,12 @@ public class EmailingService {
     @Autowired
     private final JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.from-header}")
+    private String from;
+
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
