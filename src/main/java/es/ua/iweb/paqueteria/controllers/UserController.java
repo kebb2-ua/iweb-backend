812553faba87466afe1a0a1de3a0fb19e6d37329
Kyllian Edouard Boucir <kebb2@alu.ua.es>
2025-01-16 16:19:02 +0100

    Roles

--- src/main/java/es/ua/iweb/paqueteria/controllers/UserController.java
+++ src/main/java/es/ua/iweb/paqueteria/controllers/UserController.java
@@ -7,12 +7,14 @@ import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
+@PreAuthorize("hasAuthority('ADMIN')")
 @RequiredArgsConstructor
 @CrossOrigin
 @Hidden
