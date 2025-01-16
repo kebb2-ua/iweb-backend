92ab5347d6fa9faf1c4c7db2af3c372153bb8f5c
samuel2793 <padillasamuel2793@gmail.com>
2025-01-16 15:58:33 +0100

    feat: cambiar rol de usuario

--- src/main/java/es/ua/iweb/paqueteria/controllers/UserController.java
+++ src/main/java/es/ua/iweb/paqueteria/controllers/UserController.java
@@ -2,6 +2,7 @@ package es.ua.iweb.paqueteria.controllers;
 
 import es.ua.iweb.paqueteria.dto.UserDTO;
 import es.ua.iweb.paqueteria.service.UserService;
+import es.ua.iweb.paqueteria.type.RoleType;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class UserController {
     @GetMapping("")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PatchMapping("/role")
+    public ResponseEntity<String> updateUserRole(
+            @RequestParam("email") String email,
+            @RequestParam("role") RoleType role
+    ) {
+        userService.updateUserRole(email, role);
+        return ResponseEntity.ok("Role updated successfully");
     }
 }
--- src/main/java/es/ua/iweb/paqueteria/service/UserService.java
+++ src/main/java/es/ua/iweb/paqueteria/service/UserService.java
@@ -4,6 +4,7 @@ import es.ua.iweb.paqueteria.dto.UserDTO;
 import es.ua.iweb.paqueteria.entity.UserEntity;
 import es.ua.iweb.paqueteria.exception.DataNotFoundException;
 import es.ua.iweb.paqueteria.repository.UserRepository;
+import es.ua.iweb.paqueteria.type.RoleType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,15 @@ public class UserService {
 
     public UserEntity getUserByEmail(String email) {
         return userRepository.findByEmail(email).orElseThrow(DataNotFoundException::userNotFound);
+    }
+
+    public void updateUserRole(String email, RoleType role) {
+        UserEntity user = getUserByEmail(email);
+
+        // Reemplaza todos los roles existentes por el nuevo rol.
+        user.getRolesList().clear();
+        user.getRolesList().add(role);
+
+        userRepository.save(user);
     }
 }
